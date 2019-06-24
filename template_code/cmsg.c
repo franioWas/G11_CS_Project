945e36e2cbb9fbbfcc9d6b2f81cc151a04afb417
franioWas <wfranio@yahoo.ca>
2019-06-24 16:59:44 -0400

    finished the crude modularity

--- template_code/cmsg.c
+++ template_code/cmsg.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-#include "comm.h"
-#include "client.h"
-#include "server.h"
+#include <comm.h>
+#include <client.h>
+#include <server.h>
 
-int main (){ 
 
+int main (int argc, char *argv[]) {
+	if (argc >= 2) {
+		printf("too many arguments");
+		exit(1);
+	}
+	if (argc == 1) {
+		client(argv[1]);
+	else server();
 	return 0;
 }
